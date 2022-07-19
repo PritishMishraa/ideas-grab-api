e4a953a27ede21affdba1ec2168e2e5cfc7aa9a7
Pritish Mishra <pritishmishra579@gmail.com>
2022-07-20 03:49:30 +0530

    favicon

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 /** Favicon */
 const __dirname = path.resolve();
-app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // Redirect the root URL to the github repository
 app.get('/', (_req, res) => {
